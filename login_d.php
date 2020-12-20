0d149f25d7e5314f7cbe2fc667787efef09d8c2c
rasatikah <rasatikah@graduate.utm.my>
2020-12-21 04:44:29 +0800

    deliver db correction

--- login_d.php
+++ login_d.php
@@ -22,7 +22,7 @@ if (isset($_POST['login_delivery'])) {
   
     if (count($errors) == 0) {
         $password = md5($password);
-        $query = "SELECT * FROM delivery WHERE username='$username' AND password='$password'";
+        $query = "SELECT * FROM deliver WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
